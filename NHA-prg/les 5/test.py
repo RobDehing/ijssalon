061329f4e5ca9e47f9dbbe22ac21be121ec06ffc
RobDehing <129286832+RobDehing@users.noreply.github.com>
2023-03-30 10:33:29 +0200

    Delete test.py

--- NHA-prg/les 5/test.py
+++
@@ -1,9 +0,0 @@
-for i in range(100, 0, -10):
-    print(i)
-
-for i in range(10,20):
-    print(i," maal 2 is", i*2)
-
-leeftijd = 30
-naam = "Deborah"
-print(f"Haar naam is {naam}, ze is {leeftijd} jaar oud.")
