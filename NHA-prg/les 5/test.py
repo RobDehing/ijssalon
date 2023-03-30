b636361fb857fa7f859efa509f992caa36540f90
RobDehing <rb.dhng@gmail.com>
2023-03-30 17:18:33 +0200

    update

---
+++ NHA-prg/les 5/test.py
@@ -0,0 +1,28 @@
+A = input("Welk getal wilt u verdubbelen? ")
+B = int(A)
+C = B * 2
+print(C)
+a = "maandag"
+b = "dinsdag"
+c = "woensdag"
+print(a, b, c, sep="-")
+print(f"Eerst komt {a} dan {b} en dan {c}")
+for i in range(10, 0, -1):
+    print(i)
+print("Lift off!")
+i = 0
+while i < 6:
+    print(i)
+    i = i + 1
+print("Einde")
+# huiswerkopgave 1
+aantal = 10
+prijs = 0.50
+totaal = aantal * prijs
+# huisweropgave 2
+for i in range(10,20):
+    print(i,"maal 2 is",i*2)
+# huiswerkopgave 3
+leeftijd = 30
+naam = "Deborah"
+print(f"Haar naam is {naam}, ze is {leeftijd} jaar oud.")
