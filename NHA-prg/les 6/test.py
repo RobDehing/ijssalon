0159c9b9382ff5038583f5b4cdce07cf7bbbfe77
RobDehing <rb.dhng@gmail.com>
2023-03-31 11:14:22 +0200

    Dictionary

--- NHA-prg/les 6/test.py
+++ NHA-prg/les 6/test.py
@@ -1,22 +1,13 @@
-mijn_lijst = ["wafels", "Softijs", "Schepijs", "Pannekoeken"]
-mijn_lijst.append("Muffins")
-for item in mijn_lijst:
-    print(f"Wij verkopen ", item)
-
-alfabet = ["A", "B", "D", "E", "F"]
-print(alfabet)
-alfabet.insert(2, "C")
-print(alfabet)
-fruit = ["appel", "banaan", "kers", "druif", "druif"]
-print("voor pop(1)", fruit)
-fruit.pop(1)
-print("Na pop(1)", fruit)
-fruit.remove("kers")
-print(fruit)
-my_list_1 = ["appel", 2, True, "banaan"]
-print("my-list = ", my_list_1)
-# my_list.clear()
-print("my-list = ", my_list_1)
-print(my_list_1.count(2))
-fruit.remove("druif")
-print(fruit)
+mijn_dictionary = {
+    "product" : "softijs",
+    "aantal" : 101,
+    "smaak" : "vanille"
+}
+keys = mijn_dictionary.keys()
+print(keys)
+values = mijn_dictionary.values()
+print(values)
+print(mijn_dictionary)
+print("--------------------")
+for k, v in mijn_dictionary.items():
+    print(k ,v)
